a4e120ff9ffb9a7cf6a3402c75ba3e182b3f5c04
mathieujonin-hash <mathieu.jonin@gmail.com>
2025-11-11 08:57:16 +0100

    début js

---
+++ 2_web/js/exemple.js
@@ -0,0 +1,26 @@
+var message = "Benvenuti ";
+
+//console.log(message);
+
+//function MaFonction() {
+//    console.log(message);
+//}
+
+//for (var i = 0; i < 10; i++) {
+//    console.log(message + i);
+//}
+
+//console.log(document.querySelector(".important").textContent)
+//console.log(document.getElementById("bienvenue").textContent)
+
+
+
+//Comment ajouter des choses pour le site
+function ajouter_perso() {
+    var mon_perso = "Pierre";
+
+    var NewItem = document.createElement("li");
+    var NewText = document.createTextNode(TextPerso);
+    
+    console.log(mon_perso);
+}
