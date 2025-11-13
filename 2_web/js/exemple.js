719d959a05f105396a30b6591e056a657c870363
mathieujonin-hash <mathieu.jonin@gmail.com>
2025-11-13 15:14:44 +0100

    added js and css

--- 2_web/js/exemple.js
+++ 2_web/js/exemple.js
@@ -1,26 +1,58 @@
-var message = "Benvenuti ";
-
-//console.log(message);
-
-//function MaFonction() {
-//    console.log(message);
-//}
-
-//for (var i = 0; i < 10; i++) {
-//    console.log(message + i);
-//}
-
-//console.log(document.querySelector(".important").textContent)
-//console.log(document.getElementById("bienvenue").textContent)
-
-
-
 //Comment ajouter des choses pour le site
-function ajouter_perso() {
-    var mon_perso = "Pierre";
+function AddCharacter(CallSign, classe) {
+    var new_char = {
+        name: CallSign,
+        categorie: classe,
+        level: 1,
+        skills: {},
+        LevelUp: function(){
+            this.level++;
+        },
+        create_li: function() {
+            var li_character = document.createElement("li");
+            var character_text = document.createTextNode(
+                this.name + " (" + this.categorie + ", niveau " + this.level + ")"
+            );
+            li_character.appendChild(character_text);
+            li_character.setAttribute("class", this.categorie);
 
-    var NewItem = document.createElement("li");
-    var NewText = document.createTextNode(TextPerso);
+            return li_character;
+        },
+    };
+
+    return new_char;
+};
+
+var Char1 = {
+        name: "Pierre",
+        categorie: "Worker",
+        level: 10,
+        skills: {
+            Mining: 10,
+            Foraging: 22,
+            farming: 6,
+        },
+        LevelUp: function(){
+            this.level++;
+        },
+        
+    }
+
+
+
+var Group = [
+    AddCharacter("Leslie", "Warrior"),
+    AddCharacter("Jane", "Warrior"),
+    AddCharacter("Alita", "Warrior"),
+    AddCharacter("Hiro", "Warrior"),
+]
+
+for (var i = 0; i < Group.length; i++) {
+    var perso = Group[i];
+    var li_character = perso.create_li();
     
-    console.log(mon_perso);
+    var CharacterList = document.getElementById("CharList");
+    CharacterList.appendChild(li_character);
 }
+
+console.log(Group)
