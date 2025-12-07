cdd207db9b846243c55a0ed051aa1b09d3e0b022
mathieujonin-hash <mathieu.jonin@gmail.com>
2025-12-07 15:34:05 +0100

    finished the web site

--- 2_web_exemple/js/exemple2.js
+++ 2_web_exemple/js/exemple2.js
@@ -35,11 +35,14 @@ const image = document.getElementById("image");
 const ctx_image = image.getContext("2d");
 
 const img = new Image();
-img.src = "/2_web/images/dvd-logo-svgrepo-com.svg"
+img.src = "/docs/dvd-logo-svgrepo-com.svg"
 
 function init() {
     window.requestAnimationFrame(draw);
 }
+
+var w = img.naturalWidth/4;
+var h = img.naturalHeight/4;
 
  var i = 0;
  var j = 0;
@@ -48,20 +51,20 @@ function init() {
 
 function draw() {
     ctx_image.clearRect( -10, -10, 600, 600 );
-    ctx_image.drawImage( img, 0, -40, 200, 200 );
+    ctx_image.drawImage( img, 0, -40, w, h );
     ctx_image.translate( k, l );
     if ( k == 1 ) {
-        if (i < 300) { k = 1; }
+        if (i < 500-w) { k = 1; }
         else { k = -1; i = 0; }
     } else {
-        if (i < 300) { k = -1; }
+        if (i < 500-w) { k = -1; }
         else { k = 1; i = 0; }
     }
     if ( l == 1 ) {
-        if (j < 380) { l = 1; }
+        if (j < 580-h) { l = 1; }
         else { l = -1; j = 0; }
     } else {
-        if (j < 380) { l = -1; }
+        if (j < 580-h) { l = -1; }
         else { l = 1; j = 0; }
     }
     i++
--- docs/Java.js
+++ docs/Java.js
@@ -1,3 +1,48 @@
 function toggleMenu() {
     document.getElementById("sideMenu").classList.toggle("open");
 }
+
+//animation
+
+const image = document.getElementById("image");
+const ctx_image = image.getContext("2d");
+
+const img = new Image();
+img.src = "/docs/cat-space.gif"
+
+function init() {
+    window.requestAnimationFrame(draw);
+}
+
+var w = img.naturalWidth/4;
+var h = img.naturalHeight/4;
+
+var i = 0;
+var j = 0;
+var k = 1;
+var l = 1;
+
+function draw() {
+    ctx_image.clearRect( -100, -100, 800, 800 );
+    ctx_image.drawImage( img, 0, 0, w, h );
+    ctx_image.translate( k, l );
+    if ( k == 1 ) {
+        if (i < 800-w) { k = 1; }
+        else { k = -1; i = 0; }
+    } else {
+        if (i < 800-w) { k = -1; }
+        else { k = 1; i = 0; }
+    }
+    if ( l == 1 ) {
+        if (j < 450-h) { l = 1; }
+        else { l = -1; j = 0; }
+    } else {
+        if (j < 450-h) { l = -1; }
+        else { l = 1; j = 0; }
+    }
+    i++
+    j++
+    window.requestAnimationFrame(draw);
+}
+
+init()
